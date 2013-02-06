f054c10a503453e74fecdd77cbee9f2a15051853
syakesaba <syakesaba@gmail.com>
2013-02-07 01:54:44 +0900

    解説を記入した。

--- src/ProtoSumNTest.java
+++ src/ProtoSumNTest.java
@@ -1,11 +1,11 @@
 //package org.jnetpcap.examples;
 
-import org.jnetpcap.Pcap;
+import org.jnetpcap.Pcap;//jnetpcap.jarはインストールしましたか？そのクラスパスは追加しましたか？jarのパーミッションは644が適切ですよ。javaとjavacのバージョンはあっていますか？
 import org.jnetpcap.packet.PcapPacket;
 import org.jnetpcap.packet.PcapPacketHandler;
+//↑プロトコルアナライザとして使うならこんだけのインポートで十分。
 
-//-----Protocols
-
+//*-----使用出来るプロトコル達-----*
 //import org.jnetpcap.protocol.lan.SLL;
 //import org.jnetpcap.protocol.lan.IEEESnap;
 //import org.jnetpcap.protocol.lan.IEEE802dot1q;
@@ -29,10 +29,14 @@ import org.jnetpcap.protocol.tcpip.Udp;
 //import org.jnetpcap.protocol.voip.Sip;
 //import org.jnetpcap.protocol.tcpip.Http;
 //import org.jnetpcap.protocol.application.Html;
-// SIP,HTTPなどなど・・・
-//-----
+//*-----使用出来るプロトコル達-----*
+//パケットアートでは使(わ、え)ないプロトコルはコメントアウトしている。
 
-//
+
+/**
+ * このjavaファイルはjnetpcapライブラリを試用するためのファイルですが、
+ * ちょこっといじるだけですぐに他のプログラムに転用できます。
+*/
 public class ProtoSumNTest {
     public static void main(String[] args) {
         if(args.length < 1) {
@@ -40,11 +44,14 @@ public class ProtoSumNTest {
                 System.exit(-1);
         }
         try {
-        ProtoSumN psn = new ProtoSumN(args[0]);
-        psn.openPcap();//pcapファイルのio処理
-        psn.run();//loop開始
-        System.out.println(psn.getErrbuf() );//jnetpcap内部エラー情報を表示
-        } catch (Exception e) { // エラーは個別に対処するほうがいい
+        ProtoSumN psn = new ProtoSumN(args[0]);//自作クラスを呼び出す。
+        psn.openPcap();//pcapファイルを開く関数の呼び出し
+        psn.run();//pcapファイルを読み込ループ開始。
+        System.out.println(psn.getErrbuf() );//最後にjnetpcap内部エラー情報を表示。
+        } catch (Exception e) { 
+            // エラーは個別に対処するほうがいいけど、今はしない。
+            //出てくるエラーの網羅にはIDEを使わないと多分できない。
+            //今回のパケットアートはおそらくマルチスレッドになるので、エラーの見逃しは命取りだ。
             e.printStackTrace();
             System.exit(-2);
         }
@@ -52,23 +59,26 @@ public class ProtoSumNTest {
 }
 
 class ProtoSumN {
-    private StringBuilder myErrbuf;
-    //StringBufferではなくStringBuilderなので。本格的にするならStringBuffer継承しまくる。
-    private String myFile;
-    private ProtoSumNPacketHandler myHandler;
-    private Pcap myPcap;
+    private StringBuilder myErrbuf; //コンストラクタで代入される。
+    //StringBuilderはStringBufferと同じように使える。
+    //本格的にするならStringBuffer継承しまくるのがいい。
+    private String myFile;//コンストラクタで代入される
+    private ProtoSumNPacketHandler myHandler;//コンストラクタで代入される
+    private Pcap myPcap;//Pcap型はパケットの読み込みの受け皿。
 
     ProtoSumN(String filename) {
         myErrbuf = new StringBuilder();
         myHandler = new ProtoSumNPacketHandler();
         myFile = filename;
     }
+    //コンストラクタは引数の型や組み合わせで複数作るべき。
     
     public StringBuilder getErrbuf() {
         return myErrbuf;
     }
     public void openPcap() {
         myPcap = Pcap.openOffline(myFile, myErrbuf);//pcapファイルを開く
+        //エラーを放出しない代わりにErrbufにエラー情報が書き込まれる。
         if (myPcap == null) {
             System.err.printf("Error while opening a file for capture: "
                 + myErrbuf.toString());
@@ -79,19 +89,24 @@ class ProtoSumN {
     public void run() {
         try {
             myPcap.loop(0, myHandler,"DummyUserData");
-            //1000個パケット読んでmyHandlerに渡す
+            //無限個パケット読んでmyHandlerに渡す。
+            //private final int INFINITE = 0;とでもすればわかりやすい。
+            //ユーザー定義引数は今回は使わないので、適当に埋めている。
         } finally {
-            //1000回読んだらこっちに引きこむ
+            //無限回読んだらこっちに引きこむ
             myPcap.close();//開けたら閉める
-            //PacketArt.close();
+            //PacketArt.close();パケット読み終わったらどうする？（いわゆる、弾切れ）
         }
     }
 }
 
-
-
+/**
+ * このクラスはパケットハンドラ（到着パケットいじくり関数）の一つ。
+ * こいつがjnetpcapの本体といってもいい。
+*/
 class ProtoSumNPacketHandler implements PcapPacketHandler<String> {
-    //identifierを宣言
+    //識別子を宣言
+    //定数として扱うのでfinalにしている。
     private final Ethernet ETHERNET_PACKET = new Ethernet();
     private final Arp ARP_PACKET = new Arp();
     private final L2TP L2TP_PACKET = new L2TP();
@@ -102,14 +117,18 @@ class ProtoSumNPacketHandler implements PcapPacketHandler<String> {
     private final Ip6 IP6_PACKET = new Ip6();//Typeof IPv4
     private final Tcp TCP_PACKET = new Tcp();  
     private final Udp UDP_PACKET = new Udp();
+    //汚い識別子だよなあ。なんで無駄にインスタンス作らんといかんのかなあ。
+    //最下位層からペイロードの種類読んで上のレイヤーに連続ジャンプ、とかできんのか？
     
     //ProtoSumNPacketHandler(){
         //System.sout.println("This Packet has been captured by " + user);
     //}
+    //userはパケットハンドラ呼び出し元がよこしたユーザー定義引数。
     //<String>とか、いろんなオブジェクトをパケットハンドラに持ってこれるらしい。
-    //使わんがな。
+    //今回使わんがな。
+
     public void nextPacket(PcapPacket packet,String user) {
-        //packet.scan();
+        //packet.scan();//←何につかうのか分からない関数。
         try {
             if ( packet.hasHeader(TCP_PACKET) ) {  
                 System.out.print("T");
@@ -135,8 +154,9 @@ class ProtoSumNPacketHandler implements PcapPacketHandler<String> {
                 System.out.print("?");
             }
         } catch (Exception e) {
-            e.printStackTrace();
-            //DANGER PACKET packet
+            //e.printStackTrace();
+            //System.out.print(packet)
+            //解析不能パケットを受け取ったらここに飛ぶ
         }
     }
 }
