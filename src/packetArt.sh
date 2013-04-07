07f98dd792030dd02822588c51722eb518d6f580
syakesaba <syakesaba@gmail.com>
2013-04-07 21:50:29 +0900

    どこからでも実行可能にした

--- src/packetArt.sh
+++ src/packetArt.sh
@@ -4,37 +4,44 @@
 FULLPATH="jp/ac/kansai_u/kutc/firefly/packetArt";
 MAINFILE="${FULLPATH}/Main";
 JAVAC_OPTIONS=" -encoding UTF-8";
+THIS_FILE=`basename $0`;
+THIS_DIR=`dirname $0`;
 
 alert(){
-    echo 'This Script Must be run as "PacketArt/src/$0"';
+    echo -e This Script Must be run as PacketArt/src/${THIS_FILE};
     exit -1;
 }
 
 usage(){
     echo "##########################";
-    echo " $0 compile";
+    echo -e "${THIS_FILE} compile";
     echo "           -  Compile PacketArt";
-    echo " $0 start";
+    echo -e "${THIS_FILE} start";
     echo "           -  Play PacketArt";
-    echo "";
     echo "#########################";
     exit -1;
 }
 
 check(){
-    [ `basename $(dirname ${PWD})` != "PacketArt" ] && alert;
+    pushd . > /dev/null;
+    cd "${THIS_DIR}";
     [ ! -e "${FULLPATH}" ] && alert;
+    popd > /dev/null;
 }
 
 check;
 case "$1" in
     "start" | "star" | "sta" | "run" | "play")
+        pushd . > /dev/null;
+        cd "${THIS_DIR}"
         java "${MAINFILE}";
         [ "$?" == "0" ] && exit 0;
+        popd > /dev/null;
         exit -1;
     ;;
     "compile" | "c" | "compi" | "make" | "mak")
         pushd . > /dev/null;
+        cd "${THIS_DIR}"
         cd "${FULLPATH}";
         javac ${JAVAC_OPTIONS} ./*.java;
         RET="$?";
