84e19cff3e51be6036f57ed2d0d738f5b92e1268
Lisa <lisa_kud@mail.ru>
2013-04-13 16:45:43 +0900

    IPv4処理部分作成．後はIPv4をdeposeIp()に読ませる手段の実装．

--- src/jp/ac/kansai_u/kutc/firefly/packetArt/music/PacketDisposer.java
+++ src/jp/ac/kansai_u/kutc/firefly/packetArt/music/PacketDisposer.java
@@ -33,7 +33,37 @@ public class PacketDisposer extends ProtocolHandlerBase{
 	}
 
 	public int[] disposePacket(){
-		return null;
+		
+		//何とかしてIPv4アドレスをString[]で持ってくる．
+		String[] rawip = foo();
+		String ipsentence = "";
+		
+		//splitを使って"."を処理後配列に入れたいので2度手間っぽいが一度String型にする．
+		for(int i = 0; i < rawip.length; i++){
+			ipsentence = ipsentence + rawip[i] + ".";
+		}
+		
+		//オクテットごとにString配列に格納．
+		String[] stringip = ipsentence.split("\\.");
+		
+		
+		int[] ipdata = new int[stringip.length]; //String→intの作業を行うために一時的に使用する配列．
+		int[] deposedip = new int[stringip.length]; //こいつの中身の値が後のメロディ生成に影響する．
+		
+		//IPのオクテットの数字をとにかく1桁の数字に変換する．
+		for(int j = 0; j < stringip.length; j++){
+			ipdata[j] = Integer.parseInt(stringip[j]);
+			if(ipdata[j] < 9){
+				deposedip[j] = ipdata[j];
+			}else if(ipdata[j] < 100){
+				deposedip[j] = Math.round(ipdata[j]/10);
+			}else if(ipdata[j] < 200){
+				deposedip[j] = Math.round((ipdata[j]-100)/10);
+			}else if(ipdata[j] < 256){
+				deposedip[j] = Math.round((ipdata[j]-200)/10);
+			}
+		}
+		return deposedip;
 	}
 	
 	@Override
